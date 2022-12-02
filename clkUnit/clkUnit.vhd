192dffea29241d1533ec39a9ce0518aee28003aa
Chaman Vétéran <nathm.va@gmail.com>
2022-12-02 21:00:09 +0100

    Ajout de clk + TXUnit

---
+++ clkUnit/clkUnit.vhd
@@ -0,0 +1,39 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+
+entity clkUnit is
+  
+ port (
+   clk, reset : in  std_logic;
+   enableTX   : out std_logic;
+   enableRX   : out std_logic);
+    
+end clkUnit;
+
+architecture behavorial of clkUnit is
+  signal top : std_logic := '0';
+  constant facteur : natural := 16;
+begin
+	enableRX <= clk and reset;
+	
+  div : process (clk, reset)
+    variable cpt : integer range 0 to facteur-1 := 0;
+  begin 
+    if reset = '0' then
+      enableTX <= '0';
+      cpt := 0;
+    elsif rising_edge(clk) then
+      if(cpt = facteur - 1) then
+        cpt := 0;
+      else
+        cpt := cpt + 1;
+      end if;
+      if cpt = 0 then
+        enableTX <= '1';
+      else
+        enableTX <= '0';
+      end if;
+    end if;
+  end process;
+
+end behavorial;
